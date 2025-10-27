e3ed003ef3a4caa5eade37ce36e7cb31932f9970
chogeamos <chogeamos@gmail.com>
2025-10-27 14:29:01 -0400

    Add built frontend and updated server configuration

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, "admin-app/build")));
 
 app.get("*", (req, res) => {
