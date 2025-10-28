117e051f684265e96730e7db200e18794e3de6cb
chogeamos <chogeamos@gmail.com>
2025-10-28 03:27:15 -0400

    Convert admin-app from submodule to normal folder

---
+++ admin-app/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700 mb-4">
+        Tailwind CSS is Working 🚀
+      </h1>
+      <p className="text-gray-600">
+        If you see this styled page, your setup is successful!
+      </p>
+    </div>
+  );
+}
+
+export default App;
